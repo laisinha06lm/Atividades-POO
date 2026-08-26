29a418ca5cbec45890c0bc4f75f01b5b73fe25d5
laisinha06lm <lais.lmen@gmail.com>
2026-08-26 20:13:13 -0300

    Add files via upload
    Exercício 2 tbl

---
+++ exercicio2.cs
@@ -0,0 +1,19 @@
+﻿class Pessoa
+{
+    public string Nome;
+}
+
+class Program
+{
+    static void Main()
+    {
+        Pessoa p1 = new Pessoa();
+        p1.Nome = "Maria";
+
+        Pessoa p2 = p1;
+
+        p2.Nome = "Lilian";
+
+        Console.WriteLine(p1.Nome);
+    }
+}
