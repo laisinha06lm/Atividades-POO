be4954da51b9a99d48ecbb10f17e03d483d28568
laisinha06lm <lais.lmen@gmail.com>
2026-08-26 20:23:06 -0300

    Add files via upload
    Exercício 3 tbl

---
+++ exercicio3.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main()
+    {
+        double num = 21.8;
+
+        int num_inteiro = (int)num;
+
+        Console.WriteLine($"Valor antes da conversão: {num}");
+        Console.WriteLine($"Valor depois da conversão: {num_inteiro}");
+    }
+}
