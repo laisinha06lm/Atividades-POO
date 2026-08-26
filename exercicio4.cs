af8ea91e9d89c5e9c44b53f4478a499f11ab21b9
laisinha06lm <lais.lmen@gmail.com>
2026-08-26 20:33:02 -0300

    Add files via upload
    Exercício 4 -- tbl

---
+++ exercicio4.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número: ");
+        string num = Console.ReadLine();
+
+        int resultado;
+
+        if(int.TryParse(num, out resultado))
+        {
+            Console.WriteLine($"Número digitado: {resultado}");
+        }
+        else
+        {
+            Console.WriteLine("Número inválido");
+        }
+    }
+}
