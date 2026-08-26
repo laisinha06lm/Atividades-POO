28e3be8d11ac1c6adb34154ebf21dcd7e529f7fb
laisinha06lm <lais.lmen@gmail.com>
2026-08-26 20:12:37 -0300

    Add files via upload
    Exercício 1 tbl

---
+++ exercicio1.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int num1 = 10;
+        int num2 = num1;
+
+        num2 = 45;
+
+        Console.WriteLine($"O número 1 é: {num1}");
+        Console.WriteLine($"O número 2 é: {num2}");
+
+    }
+
+}
