fccc421638134335004f5694cc842142f68bcfc1
Laís Lopes Mendes <lais.lmen@gmail.com>
2026-09-17 16:58:54 -0300

    Add files via upload
    Exercício 3 do 2º Roteiro

---
+++ Roteiros POO 1/Roteiro 2/exercício3_r2.cs
@@ -0,0 +1,69 @@
+﻿public class Elevador
+{
+    private int andarAtual;
+    private int totalAndares;
+
+    public Elevador(int totalAndares)
+    {
+        this.totalAndares = totalAndares;
+        andarAtual = 0;
+    }
+
+    public void Subir()
+    {
+        if(andarAtual < totalAndares)
+        {
+            andarAtual++;
+            Console.WriteLine($"O andar subiu para o {andarAtual}");
+        }
+        else
+        {
+            Console.WriteLine("O elevador já está no último andar.");
+        }
+    }
+
+    public void Descer()
+    {
+        if(andarAtual > 0)
+        {
+            andarAtual--;
+            Console.WriteLine($"O andar desceu para o {andarAtual}");
+        }
+        else
+        {
+            Console.WriteLine("O elevador já está no térreo.");
+        }
+    }
+
+    public void ExibirAndar()
+    {
+        Console.WriteLine($"Andar atual: {andarAtual}");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Elevador e = new Elevador(10);
+        Console.WriteLine("Teste do 1º elevador:");
+        e.Subir();
+        e.Subir();
+        e.ExibirAndar();
+        e.Descer();
+        e.ExibirAndar();
+        e.Descer();
+        e.Descer();
+        e.ExibirAndar();
+
+        Elevador e2 = new Elevador(3);
+        Console.WriteLine("Teste do 2º elevador:");
+        e2.Descer();
+        e2.ExibirAndar();
+        e2.Subir();
+        e2.Subir();
+        e2.Subir();
+        e2.Subir();
+        e2.ExibirAndar();
+    }
+}
