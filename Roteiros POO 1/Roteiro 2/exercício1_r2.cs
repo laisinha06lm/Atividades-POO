8e2488bec4b689460a866375917e2edc35b1e704
Laís Lopes Mendes <lais.lmen@gmail.com>
2026-09-17 15:59:10 -0300

    Add files via upload
    Exercício 1 do 2º Roteiro

---
+++ Roteiros POO 1/Roteiro 2/exercício1_r2.cs
@@ -0,0 +1,51 @@
+﻿public class Produto
+{
+    private string Nome;
+    private decimal Preco;
+
+    public Produto(string Nome, decimal Preco)
+    {
+        this.Nome = Nome;
+
+        if(Preco < 0)
+        {
+            Console.WriteLine("O preço não pode ser negativo. O preço será definido como 0.");
+            this.Preco = 0;
+        }
+        else
+        {
+            this.Preco = Preco;
+        }
+    }
+
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"Nome do Produto: {Nome}");
+        Console.WriteLine($"Preço do Produto: {Preco:C}");
+    }
+
+    public void AlterarPreco(decimal novoPreco)
+    {
+        if(novoPreco < 0)
+        {
+            Console.WriteLine("O preço não pode ser negativo. O preço será mantido como está.");
+        }
+        else
+        {
+            Preco = novoPreco;
+            Console.WriteLine($"Preço do produto alterado para: {Preco:C}");
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Produto p = new Produto("Celular", 1500);
+        p.ExibirDetalhes();
+        p.AlterarPreco(-200);
+        p.AlterarPreco(1200);
+        p.ExibirDetalhes();
+    }
+}
