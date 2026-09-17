d9082f7ff61e5a801b67449aed5de286036ba2c3
Laís Lopes Mendes <lais.lmen@gmail.com>
2026-09-17 16:17:30 -0300

    Add files via upload
    Exercício 2 do 2º Roteiro

---
+++ Roteiros POO 1/Roteiro 2/exercício2_r2.cs
@@ -0,0 +1,61 @@
+﻿public class Carro
+{
+    private string Modelo;
+    private int VelocidadeAtual;
+
+    public Carro(string Modelo)
+    {
+        this.Modelo = Modelo;
+        VelocidadeAtual = 0;
+    }
+
+    public void Acelerar(int valor)
+    {
+        if (valor < 0)
+        {
+            Console.WriteLine("Erro: Valor de aceleração não pode ser negativo.");
+        }
+        else
+        {
+            VelocidadeAtual += valor;
+            Console.WriteLine($"O carro {Modelo} acelerou para {VelocidadeAtual} km/h.");
+        }
+    }
+
+    public void Frear(int valor)
+    {
+        if (valor < 0)
+        {
+            Console.WriteLine("Erro: Valor de frenagem não pode ser negativo.");
+            
+        }
+        else
+        {
+            VelocidadeAtual -= valor;
+            if (VelocidadeAtual < 0)
+            {
+                VelocidadeAtual = 0;
+            }
+            Console.WriteLine($"O carro {Modelo} desacelerou para {VelocidadeAtual} km/h.");
+        }
+    }
+
+    public void ExibirVelocidade()
+    {
+        Console.WriteLine($"A velocidade atual é {VelocidadeAtual} km/h.");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Carro c = new Carro("Ferrari");
+        c.Acelerar(50);
+        c.ExibirVelocidade();
+        c.Frear(30);
+        c.ExibirVelocidade();
+        c.Frear(50);
+        c.ExibirVelocidade();
+    }
+}
